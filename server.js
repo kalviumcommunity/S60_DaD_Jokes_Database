38fb26444a29053066f6763a3734480bf85f76b8
“Rahul” <chaamrthirahul2105@gmail.com>
2024-03-14 18:05:44 +0530

    client Done

--- server.js
+++ server.js
@@ -1,14 +1,30 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
 const app = express();
 
-app.get("/", (req, res) => {
-  res.redirect("/ping");
-});
+const URI = process.env.URI;
+
+mongoose
+  .connect(URI)
+  .then(() => {
+    console.log("Connected");
+    app.get("/", (req, res) => {
+      res.redirect("/ping");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 app.get("/ping", (req, res) => {
-  res.send("🦇");
+  const isConnected = mongoose.connection.readyState === 1;
+  // Send response with database connection status
+  res.json({
+    database: isConnected ? "connected" : "disconnected",
+  });
 });
 
-app.listen(3030, () => {
+app.listen(4000, () => {
   console.log("The wagon runs");
 });
